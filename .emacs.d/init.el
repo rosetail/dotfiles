41a6a52ee1dec030b0ec5dbc1ea9b47c7744d946
Rose Osterheld <dev@roeli.org>
2020-12-23 21:50:52 -0800

    change font and other stuff

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -43,7 +43,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "#2d2d2d" :foreground "#d3d0c8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :foundry "PfEd" :family "Inconsolata LGC"))))
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "#2d2d2d" :foreground "#d3d0c8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 110 :width normal :foundry "CYRE" :family "Inconsolata"))))
  '(erc-current-nick-face ((t (:inherit font-lock-keyword-face))))
  '(erc-nick-default-face ((t (:inherit font-lock-function-name-face :weight normal))))
  '(erc-prompt-face ((t (:foreground "#ffcc66"))))
